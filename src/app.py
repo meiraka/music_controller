5a7aa75178e6a0f4a9c494f9bcab963e4ff2c776
mei raka <meiraka.pre@gmail.com>
2012-11-18 23:29:39 +0900

    bind playlist changes event

--- src/app.py
+++ src/app.py
@@ -2,24 +2,29 @@ import wx
 
 import client
 import frame
+import environment
 
 class App(wx.App):
 	"""
 	this app class.
 	"""
-	def __init__(self):
+	def __init__(self,**params):
 		"""init this app"""
+		self.config_dir = environment.config_dir
+		self.client = client.Client(self.config_dir)
 		wx.App.__init__(self)
-		self.client = None
-		pass
 
 	def connect_default(self):
+		self.client.config.profiles = [[u'server',u'192.168.0.4',u'6600',False,u'']]
 		self.client.connect()
+
+	def MainLoop(self):
+		wx.App.MainLoop(self)
 
 	def OnInit(self):
 		""" Event func when app init
 		"""
-		self.client = client.Client()
+
 		self.connect_default()
 		self.frame = frame.Frame(None,self.client)
 		self.frame.Show()
@@ -30,8 +35,6 @@ class App(wx.App):
 		
 		
 def run(**params):
-	app = App()
-	for attr, value in params.iteritems():
-		setattr(app,attr,value)
+	app = App(**params)
 	app.MainLoop()
 	
--- src/playlist.py
+++ src/playlist.py
@@ -6,13 +6,19 @@ class Playlist(wx.ListBox):
 	def __init__(self,parent,playlist):
 		wx.ListBox.__init__(self,parent,-1)
 		self.playlist = playlist
-
 		self.update_playlist()
+		self.playlist.bind(self.playlist.UPDATE,self.update_playlist)
 
-	def update_playlist(self):
+	def update_playlist(self,*args,**kwargs):
+		wx.CallAfter(self.__update_playlist)
+
+	def __update_playlist(self):
 		self.Clear()
 		songs = [self._generate_title(song) for song in self.playlist]
 		self.InsertItems(songs,0)
 
 	def _generate_title(self,song):
-		return '%(artist)s - %(title)s' % song
+		try:
+			return u'%(artist)s - %(title)s' % song
+		except Exception,err:
+			return str(err)
