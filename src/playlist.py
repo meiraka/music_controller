6e59352d7c42afdd16745bb1104faaf6226e38ea
mei raka <meiraka.pre@gmail.com>
2012-11-24 00:18:36 +0900

    add event: play a song which double clicked

--- src/playlist.py
+++ src/playlist.py
@@ -3,19 +3,27 @@
 import wx
 
 class Playlist(wx.ListBox):
-	def __init__(self,parent,playlist):
+	def __init__(self,parent,playlist,debug=False):
 		wx.ListBox.__init__(self,parent,-1)
 		self.playlist = playlist
+		self.__debug = debug
 		self.update_playlist()
 		self.playlist.bind(self.playlist.UPDATE,self.update_playlist)
+		self.Bind(wx.EVT_LISTBOX_DCLICK,self.OnActivate)
+
+	def OnActivate(self,event):
+		index = event.GetInt()
+		self.playlist[index].play()
 
 	def update_playlist(self,*args,**kwargs):
+		if self.__debug: print 'update_playlist'
 		wx.CallAfter(self.__update_playlist)
 
 	def __update_playlist(self):
 		self.Clear()
 		songs = [self._generate_title(song) for song in self.playlist]
-		self.InsertItems(songs,0)
+		if songs:
+			self.InsertItems(songs,0)
 
 	def _generate_title(self,song):
 		try:
