23a66b67008f0f0b854ae417b5525052590b15e1
mei raka <meiraka.pre@gmail.com>
2012-11-26 01:52:17 +0900

    add info

--- src/frame.py
+++ src/frame.py
@@ -12,12 +12,18 @@ class Frame(wx.Frame):
 		self.parent = parent
 		self.client = client
 		wx.Frame.__init__(self,parent,-1)
+		self.SetSize((640,480))
+
 		self.toolbar = toolbar.Toolbar(self,self.client.playback)
 		self.playlist = playlist.Playlist(self,self.client.playlist,debug)
-		#self.info = info.Info(self,self.client,debug)
+		self.info = info.Info(self,self.client,debug)
 		self.albumlist = albumlist.AlbumList(self,self.client.playlist,debug)
 
 		self.sizer = wx.BoxSizer()
-		self.sizer.Add(self.playlist,2,flag=wx.EXPAND)
+		s = wx.BoxSizer(wx.VERTICAL)
+		s.Add(self.info,0,wx.EXPAND)
+		s.Add(self.playlist,1,flag=wx.EXPAND)
+		self.sizer.Add(s,1,flag=wx.EXPAND)
 		self.sizer.Add(self.albumlist,0,flag=wx.EXPAND)
 		self.SetSizer(self.sizer)
+		
