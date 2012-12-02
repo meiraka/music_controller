e49fbf371b2981fcc29db730d6d436b96ba8c6f0
mei raka <meiraka.pre@gmail.com>
2012-12-02 20:57:03 +0900

    add playlist / library switcher

--- src/frame.py
+++ src/frame.py
@@ -13,11 +13,15 @@ class Frame(wx.Frame):
 		self.client = client
 		wx.Frame.__init__(self,parent,-1)
 		self.SetSize((640,480))
+		if debug: print 'init frame'
 
 		self.toolbar = toolbar.Toolbar(self,self.client.playback)
+		if debug: print 'init playlist'
 		self.playlist = playlist.Playlist(self,self.client.playlist,self.client.playback,debug)
+		if debug: print 'init library'
 		self.library = library.Library(self,self.client.library,self.client.playlist,debug)
 		#self.info = info.Info(self,self.client,debug)
+		if debug: print 'sizing'
 
 		self.sizer = wx.BoxSizer()
 		s = wx.BoxSizer(wx.VERTICAL)
@@ -26,8 +30,21 @@ class Frame(wx.Frame):
 		s.Add(self.library,1,flag=wx.EXPAND)
 		self.sizer.Add(s,1,flag=wx.EXPAND)
 		#self.sizer.Add(self.albumlist,0,flag=wx.EXPAND)
+		self.library.Hide()
 		self.SetSizer(self.sizer)
+		if debug: print 'sized.'
 		self.client.playback.bind(self.client.playback.UPDATE_PLAYING,self.change_title)
+		if debug: print 'binded.'
+
+	def show_library(self):
+		self.playlist.Hide()
+		self.library.Show()
+		self.Layout()
+	
+	def show_playlist(self):
+		self.library.Hide()
+		self.playlist.Show()
+		self.Layout()
 
 	def change_title(self):
 		wx.CallAfter(self.__change_title)
