f34723b02cdc4ead560255fcd3dfd4de2f2b353f
mei raka <meiraka.pre@gmail.com>
2012-12-05 21:12:13 +0900

    get font and font_height

--- src/environment.py
+++ src/environment.py
@@ -9,3 +9,33 @@ elif wx.PlatformInfo[1] == 'wxGTK':
 	gui = 'gtk'
 else:
 	gui = ''
+
+class __UI(object):
+	"""
+	params for ui.*
+	"""
+	__cached = {}
+	def __get_font(self):
+		key = u'font'
+		if self.__cached.has_key(key):
+			return self.__cached[key]
+		else:
+			font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT )
+			self.__cached[key] = font
+			return self.__cached[key]
+	
+	def __get_text_height(self):
+		if self.__cached.has_key(u'text_height'):
+			return self.__cached[u'text_height']
+		else:
+			image = wx.EmptyBitmap(100,100)
+			image_writer = wx.MemoryDC(image)
+			image_writer.SetFont(self.font)
+			text_height = image_writer.GetTextExtent('A-glFf')[1]
+			self.__cached[u'text_height'] = text_height
+			return self.__cached[u'text_height']
+
+	font = property(__get_font)
+	text_height = property(__get_text_height)
+
+ui = __UI()
