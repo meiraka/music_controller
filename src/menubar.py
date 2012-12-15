bacf9d00f9368d17c6142527674ab56b6533de62
mei raka <meiraka.pre@gmail.com>
2012-12-15 21:06:49 +0900

    add menubar items

--- src/menubar.py
+++ src/menubar.py
@@ -3,6 +3,9 @@ import wx
 import preferences
 
 class MenuBar(wx.MenuBar):
+	NORMAL = 'normal'
+	TOGGLE = 'toggle'
+	SPLITTER = 'splitter'
 	def __init__(self,parent,client,accele=True):
 		wx.MenuBar.__init__(self)
 		self.accele = accele
@@ -10,7 +13,18 @@ class MenuBar(wx.MenuBar):
 		self.client = client
 		self.menu_list = [
 			('Edit',[
-				(wx.ID_PREFERENCES,u'Preferences')
+				(wx.ID_PREFERENCES,u'Preferences',self.NORMAL)
+				]),
+			('Playback',[
+				(wx.NewId(),u'Play',self.NORMAL),
+				(wx.NewId(),u'Pause',self.NORMAL),
+				(wx.NewId(),u'Stop',self.NORMAL),
+				(wx.NewId(),u'Previous',self.NORMAL),
+				(wx.NewId(),u'Next',self.NORMAL),
+				(wx.NewId(),u'splitter',self.SPLITTER),
+				(wx.NewId(),u'Shuffle',self.TOGGLE),
+				(wx.NewId(),u'Repeate',self.TOGGLE),
+				(wx.NewId(),u'Repeate one',self.TOGGLE),
 				])
 			]
 
@@ -27,9 +41,10 @@ class MenuBar(wx.MenuBar):
 			menu = wx.Menu()
 			menu.Bind(wx.EVT_MENU,self.OnMenu)
 			self.Append(menu,head)
-			for id,label in items:
-				self.__ids[id] = head+u'_'+label
-				menu.Append(id,label)
+			for id,label,menu_type in items:
+				if menu_type == self.NORMAL:
+					self.__ids[id] = head+u'_'+label
+					menu.Append(id,label)
 		self.parent.Bind(wx.EVT_MENU,self.OnMenu)
 		self.set_accelerator_table(self.__keys)
 		self.set_menu_accelerator(self.__keys,self.accele)
@@ -37,33 +52,42 @@ class MenuBar(wx.MenuBar):
 	def set_accelerator_table(self,keys):
 		key_tables = dict(Ctrl=wx.ACCEL_CTRL)
 		table = []
-		for head,items in self.menu_list:
-			for id,label in items:
-				key = keys[head+u'_'+label]
-				splitted = key.split('+')
-				flags = None
-				keycode = None
-				for i in splitted:
-					if key_tables.has_key(i):
-						if flags == None:
-							flags = key_tables[i]
-						else:
-							flags = flags | key_tables[i]
+		def get_key(key,head,id,label):
+			if not keys.has_key(head+u'_'+label):
+				return (None,None)
+			key = keys[head+u'_'+label]
+			splitted = key.split('+')
+			flags = None
+			keycode = None
+			for i in splitted:
+				if key_tables.has_key(i):
+					if flags == None:
+						flags = key_tables[i]
 					else:
-						keycode = ord(i)
-				if not flags:
-					flags = wx.ACCEL_NORNAL
-				if keycode:
-					table.append((flags,keycode,id))
+						flags = flags | key_tables[i]
+				else:
+					keycode = ord(i)
+			if not flags:
+				flags = wx.ACCEL_NORNAL
+			return (flags,keycode)
+
+		for head,items in self.menu_list:
+			for id,label,menu_type in items:
+				if not menu_type == self.SPLITTER:
+					flags,keycode = get_key(keys,head,id,label)
+					if keycode:
+						table.append((flags,keycode,id))
 		self.parent.SetAcceleratorTable(wx.AcceleratorTable(table))
 				
 	def set_menu_accelerator(self,keys,accele):
 		for index,(head,items) in enumerate(self.menu_list):
 			menu = self.GetMenu(index)
 			self.SetLabelTop(index,self.menu_label(head,accele))
-			for id,label in items:
-				key = keys[head+u'_'+label]
-				menu.SetLabel(id,self.menu_label(label,accele)+u'\t'+key)
+			for id,label,menu_type in items:
+				if not menu_type == self.SPLITTER:
+					if keys.has_key(head+u'_'+label):
+						key = keys[head+u'_'+label]
+						menu.SetLabel(id,self.menu_label(label,accele)+u'\t'+key)
 
 	def menu_label(self,label,accele):
 		if accele:
