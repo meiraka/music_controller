145be8fa09e155936438383670a8fd1fd29721ad
mei raka <meiraka.pre@gmail.com>
2013-01-27 22:27:25 +0900

    refactoring.

--- src/__init__.py
+++ src/__init__.py
@@ -10,5 +10,9 @@ from version import __version__
 from app import App
 from app import run
 
+
+"""
+Main Application.
+"""
 class App(App):
 	pass
--- src/app.py
+++ src/app.py
@@ -9,9 +9,13 @@ import gettext
 
 NAME = 'MusicController'
 
+"""
+Main Application.
+"""
 class App(wx.App):
-	"""
-	this app class.
+	""" Initializes Application.
+
+	loads config directory and installs language.
 	"""
 	def __init__(self,**params):
 		"""init this app"""
@@ -28,9 +32,11 @@ class App(wx.App):
 		wx.App.__init__(self)
 
 	def connect_default(self):
+		""" Connects to default host and runs mpd monitor daemon in background."""
 		thread.start_new_thread(self.__connect_default,())
 
 	def __connect_default(self):
+		""" Connects to default host and runs mpd monitor daemon."""
 		if self.__debug: print 'connect to default host..'
 		if self.client.connect():
 			if self.__debug: print 'connected.'
@@ -46,12 +52,15 @@ class App(wx.App):
 					if self.__debug: print 'fail! (>_<)'
 			else:
 				wx.CallAfter(self.frame.show_connection)
+		# run monitor daemon
 		self.client.start()
 
 	def reconnect(self):
+		""" Reconnects to previously connected host in background."""
 		thread.start_new_thread(self.__reconnect,())
 
 	def __reconnect(self):
+		""" Reconnects to previously connected host."""
 		if self.__debug: print 'reconnecting...',
 		time.sleep(3)
 		if self.client.connect():
@@ -62,9 +71,11 @@ class App(wx.App):
 			wx.CallAfter(self.frame.show_connection)
 
 	def connected(self):
+		""" Hides connection panel."""
 		wx.CallAfter(self.frame.show_not_connection)
 
 	def MainLoop(self):
+		""" Runs Application mainloop."""
 		wx.App.MainLoop(self)
 
 	def OnInit(self):
@@ -83,18 +94,32 @@ class App(wx.App):
 		return True
 
 	def OnClose(self,event):
+		""" catch window close event.
+		close event is raises in Mac environment only. Hide window.
+		"""
 		self.frame.Hide()
 
 	def OnActivate(self,event):
+		""" catch application activate event.
+		application activate event is raises in Mac environment only.
+		Show window.
+		"""
 		self.frame.Show()
 
 	def MacReopenApp(self):
+		""" Show window.
+		this function is called when App icon was clicked in Mac environment.
+		"""
 		self.frame.Show()
 
 	def exit(self):
+		""" Close connection."""
 		self.client.connection.close()
 		
-		
+
+"""
+Runs Application.
+"""		
 def run(**params):
 	wx.Log_EnableLogging(False)
 	app = App(**params)
