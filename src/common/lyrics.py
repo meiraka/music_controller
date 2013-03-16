082770acf0a092e42847306c7b6d3cc39d174a59
meiraka <meiraka.pre@gmail.com>
2013-03-17 00:15:24 +0900

    refactoring.

--- src/common/lyrics.py
+++ src/common/lyrics.py
@@ -13,67 +13,8 @@ import thread
 import re
 
 import client
+import rest
 import environment
-
-class Downloader(object):
-	def list(self,**kwargs):
-		""" Returns list for format() or get() arg.
-
-		Normally returns lyric url.
-		"""
-		pass
-
-	def format(self,list_returns_line):
-		""" Convert given arg to readable string.
-		"""
-		return unicode(list_returns_line)
-
-	def get(self,list_returns_line):
-		""" Returns lyric from url in given arg data.
-		"""
-		pass
-
-	def download(self,song):
-		""" Returns lyric by given song data.
-		"""
-		items = self.list(title=song.format('%title%'),
-				artist=song.format('%artist%'),
-				album = song.format('%album%'))
-		if items:
-			time.sleep(1)
-			return self.get(items[0])
-
-
-class DownloaderGeciMe(Downloader):
-	def list(self,**kwargs):
-		if not 'title' in kwargs:
-			return []
-		query_text = kwargs['title'].replace(u'/',u'*').encode('utf8')
-		if 'artist' in kwargs:
-			artist = kwargs['artist'].replace(u'/',u'*').encode('utf8')
-			query_text = query_text + '/' + artist
-		query = urllib.quote(query_text)
-		try:
-			json_text = urllib2.urlopen('http://geci.me/api/lyric/'+query).read()
-		except urllib2.URLError,err:
-			print 'can not access:','http://geci.me/api/lyric/'+query,err
-			return []
-		json_parsed = json.loads(json_text.decode('utf8'))
-		if u'result' in json_parsed:
-			return json_parsed[u'result']
-		else:
-			return []
-
-	def get(self,list_returns_line):
-		try:
-			lyric_page = urllib2.urlopen(list_returns_line[u'lrc'])
-		except urllib2.URLError,err:
-			print 'can not access:',list_returns_line[u'lrc'],err
-			return u''
-		lyric_encode = lyric_page.info()
-		lyric = lyric_page.read().decode('utf8')
-		return lyric
-
 
 
 class Database(client.Object):
@@ -209,7 +150,7 @@ class Database(client.Object):
 		return lyric
 
 	def download_from_geci_me(self,song):
-		downloader = DownloaderGeciMe()
+		downloader = rest.GeciMe()
 		return downloader.download(song)
 
 	downloading = property(lambda self:self.__downloading)
