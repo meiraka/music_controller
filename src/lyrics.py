ffeb680931ec6c3e137ff1591f749dab739f5561
meiraka <meiraka.pre@gmail.com>
2013-03-30 00:58:49 +0900

    fix bug: when end of song, lyric view shows start pos lyric line. lyric view scrolls immediately when target scroll pos is bigger than view height.

--- src/lyrics.py
+++ src/lyrics.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 """
-Draw lyric 
+Lyric view, menu and dialogs.
+
 """
 
 import sqlite3
@@ -20,7 +21,7 @@ import dialog
 
 class LyricView(wx.Panel):
 	"""
-	Draw lyric.
+	Lyric view.
 	"""
 	def __init__(self,parent,client):
 		self.client = client
@@ -99,10 +100,19 @@ class LyricView(wx.Panel):
 			for i in self.__decode_line(line):
 				result.append(i)
 		result.sort()
+		if 'time' in song and song['time'].isdigit():
+			result.append((float(song['time']),''))
 		self.__lyric = result
 
 	def __decode_line(self,line):
-		""" convert raw LRC lyric line to tuple."""
+		""" convert raw LRC lyric line to tuple.
+
+		Supports 3 types:
+			[00:00.00]
+			[00:00:00]
+			[00:00]
+		currently, karaoke tag is not supported.
+		"""
 		def convert_sec(time):
 			sec = float(time[0])*60.0+float(time[1])
 			if len(time) > 2:
@@ -174,9 +184,9 @@ class LyricView(wx.Panel):
 				current_line = index-1
 				if guess_count>0:
 					add_offset = (index*height-self.__offset)/guess_count
-					if add_offset > height:
+					if add_offset > height or abs(index*height - self.__offset) > h:
 						# too far from current pos, jump.
-						self.__offset = (index)*height
+						self.__offset = index*height
 					else:
 						self.__offset = self.__offset + add_offset 
 				break
@@ -203,6 +213,9 @@ class LyricView(wx.Panel):
 		self.PopupMenu(Menu(self,self.__song))
 
 class Menu(wx.Menu):
+	"""
+	Lyric menu.
+	"""
 	def __init__(self,parent,song):
 		wx.Menu.__init__(self)
 		self.parent = parent
@@ -227,7 +240,7 @@ class Menu(wx.Menu):
 		
 class Editor(wx.Frame):
 	"""
-	Lyric editor.
+	Lyric editor dialog.
 
 	Supports normal text edit and timetag writer mode.
 	"""
@@ -364,6 +377,9 @@ class Editor(wx.Frame):
 
 
 class Downloader(wx.Frame):
+	"""
+	Lyric Download dialog.
+	"""
 	def __init__(self,parent,client,database,song):
 		self.parent = parent
 		self.client = client
